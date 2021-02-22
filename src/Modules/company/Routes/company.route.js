ed1e3aafe2901bb1d7b9945697de9ee1b3554c39
jacob1design <mercihabam@gmail.com>
2021-02-22 14:17:35 +0100

    add create company function

--- src/Modules/company/Routes/company.route.js
+++ src/Modules/company/Routes/company.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { checkToken } = require("../../../Utils/authentication");
 const { createCompany } = require("../controllers/company.controller");
+const { checkCompanyName, checkCompanyTel, checkRequiredFields } = require("../Validation/company.validation");
 const companyRouter = express.Router();
 
-companyRouter.post("/create-company", createCompany);
+companyRouter.post("/create-company", checkToken, checkRequiredFields, checkCompanyName, checkCompanyTel, createCompany);
 
 module.exports = companyRouter;
