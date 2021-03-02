4eddf4f48382663720de0e075c2f06b2d54d0f06
jacob1design <mercihabam@gmail.com>
2021-03-02 21:46:41 +0100

    add aasociation in cart-item table with cart

--- src/Database/associations/association.js
+++ src/Database/associations/association.js
@@ -2,6 +2,7 @@ const Menus = require("../models/menus");
 const Companys = require("../models/companys");
 const cartitem = require("../models/cartItems");
 const User = require("../models/users");
+const Cart = require("../models/cart");
 
 //menus with companys
 Companys.hasMany(Menus, { as: "Menus", foreignKey: "companyId" });
@@ -14,3 +15,8 @@ cartitem.belongsTo(Menus, { as: "Menu", foreignKey: "menuId" });
 //user with cart-items
 User.hasMany(cartitem, { as: "Items", foreignKey: "userId" });
 cartitem.belongsTo(User, { as: "User", foreignKey: "userId" });
+
+Cart.hasMany(cartitem, { as: "Items", foreignKey: "cartId" });
+cartitem.belongsTo(Cart, { as: "Cart", foreignKey: "cartId" });
+Menus.hasMany(cartitem, { as: "Item", foreignKey: "menuId" });
+cartitem.belongsTo(Menu, { as: "Menu", foreignKey: "menuId" });
