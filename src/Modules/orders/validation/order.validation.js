f590f09ac55a4a82ccea40904a2c9ebe44aef39b
jacob1design <mercihabam@gmail.com>
2021-03-07 12:45:09 +0100

    correct error

--- src/Modules/orders/validation/order.validation.js
+++ src/Modules/orders/validation/order.validation.js
@@ -3,10 +3,10 @@ const { sendResult } = require("../../../Utils/helper");
 function checkRequiredFields(req, res, next){
     const { cartArray, adress, tel } = req.body;
     if(cartArray, adress, tel){
-        if(typeof(cartArray) === Array){
+        if(cartArray.length){
             next();
         }else{
-            sendResult(res, 403, "cartArray must be type of array", null, null)
+            sendResult(res, 403, `cartArray must be type of array but is ${ typeof(cartArray) }`, null, null)
         }
     }else{
         sendResult(res, 403, "vous devez rewmplir tous les champs obligatoires")
