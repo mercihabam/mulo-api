5f58774aa5845e40b9fa245b5260a6f98896b4e7
jacob1design <mercihabam@gmail.com>
2021-04-22 01:15:02 +0200

    add cors middlware

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const corsOptions = {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
-      callback(new Error('Not allowed by CORS'))
+      callback(new Error('origin' + origin + 'Not allowed by CORS'))
     }
   }
 }
