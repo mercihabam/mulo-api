674f59d47c47b83ebbd425b7c31a26c842b6f3ce
jacob1design <mercihabam@gmail.com>
2021-04-24 16:32:12 +0200

    secure cookie

--- src/Modules/Users/controllers/users.controllers.js
+++ src/Modules/Users/controllers/users.controllers.js
@@ -31,7 +31,7 @@ async function login(req, res){
         if(passwordMatch){
             const token = createToken(user.id);
             // const cookie = createCookie(res, token);
-            const cookie = res.cookie("authCookie", token, { maxAge: 86400000, httpOnly: true });
+            const cookie = res.cookie("authCookie", token, { maxAge: 86400000, httpOnly: true, secure: true });
             console.log("token is", token);
             console.log("cookie is", cookie);
             sendResult(res, 200, null, "vous avez été connecté", user)
