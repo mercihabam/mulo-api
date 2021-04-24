513820a0a2cf17f00c91aa6b81eaf26072914326
jacob1design <mercihabam@gmail.com>
2021-04-24 23:52:25 +0200

    disable cors-middlware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const Routes = require("./src/Routes");
 const dotenv = require("dotenv");
 // const bodyParser = require('body-parser');
-const cors = require("cors");
+// const cors = require("cors");
 
 dotenv.config();
 
@@ -14,23 +14,23 @@ require("./src/Database/connection/connection");
 //association
 require("./src/Database/associations/association");
 
-const whitelist = ['https://mulo-food.herokuapp.com', 'http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('origin' + origin + 'Not allowed by CORS'))
-    }
-  },
-  credentials: true
-}
+// const whitelist = ['https://mulo-food.herokuapp.com', 'http://localhost:3000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('origin' + origin + 'Not allowed by CORS'))
+//     }
+//   },
+//   credentials: true
+// }
 
 //middlwares
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use("/public", express.static('public'));
 
 //Routes
