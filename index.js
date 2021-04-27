11edbf6dbe7ee6615848ce49f7dd9f8b92727f0e
jacob1design <mercihabam@gmail.com>
2021-04-27 13:39:34 +0200

    delete path

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
-    }else if(path){
-      callback(null, true)
     }else {
       callback(new Error('origin ' +  origin + ' Not allowed by CORS'))
     }
