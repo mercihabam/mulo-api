fbbdcc129ee105dbee7dd18125e5e385165b8a7b
Merci Jacob <mercihabam@gmail.com>
2021-05-08 16:27:59 +0200

    change mail-port

--- src/Modules/Mail/mail.service.js
+++ src/Modules/Mail/mail.service.js
@@ -14,8 +14,8 @@ async function sendOrderToAdmin(codeDelivery, number) {
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: process.env.MAIL_PORT,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_ACCOUNT, // generated ethereal user
       pass: process.env.EMAIL_PASS, // generated ethereal password
