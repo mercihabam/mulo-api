9f20a1010d4e280ddf5172be7c8c4f5492c25d86
Merci Jacob <mercihabam@gmail.com>
2021-07-22 23:00:45 +0200

    change cors

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use("/public", express.static('public'));
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Routes
 app.use("/Api/v1", Routes);
